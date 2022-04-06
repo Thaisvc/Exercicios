bc39699265d7010da119f5c38c88e5a9073ca1d4
thais <thaisviana081@outlook.com>
2022-04-06 19:28:39 -0300

    atividade -1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/atividade-1.js
@@ -0,0 +1,24 @@
+const myName = "thais";
+const birthCity = "vespasiano";
+let birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity ="belo horizonte";
+console.log(birthCity);
+
+
+/* EXERCICIO
+
+1- Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+2- Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+3- Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+4- Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+4- Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+6- Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔 */
