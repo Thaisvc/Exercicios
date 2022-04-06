d74bb3aba0d2d03e419610bdf5b44397b7bbc0e6
thais <thaisviana081@outlook.com>
2022-04-06 20:30:07 -0300

    atividade-5

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/atividade-5.js
@@ -0,0 +1,29 @@
+let weekDay =  "quarta-feira";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" ||  weekDay == "quinta-feira" ||  weekDay == "sexta-feira"){
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else {
+    console.log( "FINALMENTE, descanso merecido UwU");
+
+}
+
+
+/*GABARITO
+Operador OR
+Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
+Solução:
+
+const weekDay = "quarta-feira";
+Implemente condicionais para que:
+Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
+Se for algum dia de fim de semana, imprima "FINALMENTE, descanso merecido UwU".
+Solução:
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+ console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, descando merecido UwU");
+}
+*/
