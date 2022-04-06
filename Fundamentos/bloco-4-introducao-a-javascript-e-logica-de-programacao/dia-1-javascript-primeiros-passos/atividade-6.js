210f5908715fc02b53f00b929cd0386348da781b
thais <thaisviana081@outlook.com>
2022-04-06 20:40:00 -0300

    atividade-6

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/atividade-6.js
@@ -0,0 +1,18 @@
+let status = "nao se aplica";
+
+switch (status) {
+  case "aprovada":
+    console.log("voce foi aprovada");
+    break;
+
+  case "reprovada":
+    console.log("voce foi reprovada");
+    break;
+
+    case "lista":
+    console.log("voce esta na lista de espera") ;
+    break;
+    default :
+    console.log ('não se aplica' );
+    break
+}
