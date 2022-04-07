304480209926645f9e632ad4dd76efa78a525cf5
thais <thaisviana081@outlook.com>
2022-04-07 16:00:51 -0300

    atividade-2

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/atividade-2.js
@@ -0,0 +1,6 @@
+/*1- Utilize o for para imprimir os elementos da lista groceryList com o console.log() :*/
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let list = 0; list < groceryList.length; list += 1 ){
+    console.log(groceryList[list])
+}
+
