86606d49a01051db6b3bca6bbecae4f0b5b15699
thais <thaisviana081@outlook.com>
2022-04-13 20:01:38 -0300

    atividade 2

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script-atividade2.js
@@ -0,0 +1,10 @@
+document.querySelector('h1').style.color = "white";
+document.querySelector('h1').style.backgroundColor = "green";
+document.querySelector('section').style.backgroundColor = "salmon";
+document.querySelectorAll('h3')[0].style.backgroundColor = 'pink';
+document.querySelectorAll('h3')[1].style.backgroundColor = 'pink';
+document.querySelector('.no-emergency-tasks', 'section').style.backgroundColor = "yellow";
+document.querySelectorAll('h3')[2].style.backgroundColor = 'black';
+document.querySelectorAll('h3')[3].style.backgroundColor = 'black';
+document.querySelector('#footer-container').style.backgroundColor = "green";
+
