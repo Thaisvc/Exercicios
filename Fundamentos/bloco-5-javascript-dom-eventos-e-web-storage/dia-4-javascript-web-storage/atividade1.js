626fdc3202fb4666229e7c903edeb84111bd0f70
thais <thaisviana081@outlook.com>
2022-04-19 20:00:36 -0300

    button

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/atividade1.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/atividade1.js
@@ -1,4 +1,7 @@
-function corFundo() {
-    
-    
+
+function corFundo(color) {
+   let selectElement = document.querySelector('.cropo');
+   selectElement.style.backgroundColor = color
+   localStorage.setItem("backgroundColor", color)
 }
+
