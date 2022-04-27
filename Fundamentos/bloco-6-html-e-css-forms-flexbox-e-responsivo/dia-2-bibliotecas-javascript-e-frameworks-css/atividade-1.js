4fa3e948a101709c8760f4b3c01f137acf93e5a1
thais <thaisviana081@outlook.com>
2022-04-27 14:05:44 -0300

    ajuste

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/atividade-1.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/atividade-1.js
@@ -31,6 +31,29 @@ inputCheckbox.addEventListener('change', ()=> {
 
 })
 
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
 
 
 
