e8200759a378f8d1c776ae6cf662483047d57d28
thais <thaisviana081@outlook.com>
2022-05-10 13:55:02 -0300

    atividade 2

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myRemove.test.js
@@ -0,0 +1,17 @@
+/* 2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+1- Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+2- Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+3- Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+ */
+const myRemove = require('./myRemove');
+
+
+describe('remove item do arr', () => {
+    it('retorna uma cópia desse array sem o elemento item caso ele exista no array', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
+    
+
+});
