f8972fd3b37f7741ebcb5435f63f9657cb2567b9
thais <thaisviana081@outlook.com>
2022-05-13 10:08:05 -0300

    muto triste

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/treino.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/treino.js
@@ -1,18 +1,15 @@
 //CALLBACK
-const sum = (num1,num2) => num1 + num2;
-const sub = (num1,num2) => num1 - num2;
-const mul = (num1,num2) => num1 * num2;
-const div = (num1,num2) => num1 / num2;
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mul = (num1, num2) => num1 * num2;
+const div = (num1, num2) => num1 / num2;
 
-const callback = (func) => func(10,5)
-
-console.log(callback(div));
+const callback = (func) => func(10, 5)
 
 
-
-const acorda = () => 'Acordando!!' ;
-const cafe = () => 'Bora tomar café!!' ;
-const dormir = () => 'Partiu dormir!!' ;
+const acorda = () => 'Acordando!!';
+const cafe = () => 'Bora tomar café!!';
+const dormir = () => 'Partiu dormir!!';
 
 const doingThings = (callback) => callback();
 
@@ -23,32 +20,37 @@ console.log(doingThings(acorda));
 
 const newEmployees = (gerarEmail) => {
     const employees = {
-      id1: gerarEmail('Pedro Guerra'),
-      id2:gerarEmail('Luiza Drumond'),
-      id3: gerarEmail('Carla Paiva'),
+        id1: gerarEmail('Pedro Guerra'),
+        id2: gerarEmail('Luiza Drumond'),
+        id3: gerarEmail('Carla Paiva'),
     }
     return employees;
-  };
+};
 // CALBACK RECEBE COMO PARAMETRO newEmployees/employees
-  const gerarEmail = (callbak) =>{
-      const email = callbak.toLowerCase().split(' ').join('_');
-      let gerar = { callbak, email :`${email}@gmail.com` };
-      return gerar;
-  }
+const gerarEmail = (callbak) => {
+    const email = callbak.toLowerCase().split(' ').join('_');
+    let gerar = { callbak, email: `${email}@gmail.com` };
+    return gerar;
+}
 
-  console.log(newEmployees(gerarEmail));
-
-  const livro = (books) => { books :`${books}amo de mais `};
+console.log(newEmployees(gerarEmail));
 
 
-  const books = (gerarbook) => {
-    const empogou = {
-      id1: gerarbook('trono de vidro'),
-      id2:gerarbook('nevenight'),
-      id3: gerarbook('memorias'),
+
+
+
+
+const autor = (param) => {
+    const detalhe = {
+        autor: `Jay Kristoff -> ${param}`,
+        Editora: `VR Editora -> ${param}`,
+        publicação: `1 janeiro 2017 -> ${param}`,
     }
-    return empogou;
-  };
-  console.log(books());
+    return detalhe;
+}
 
-  console.log(livro(livro));
+const book = (preferido) => preferido('Nevernight')
+
+console.log(book(autor));
+//-------------------------------------------------------------------------------------------------------------
+
