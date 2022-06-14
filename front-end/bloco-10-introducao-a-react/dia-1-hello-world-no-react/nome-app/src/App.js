4f6f83c747d380b892f1106adead0cf70478878f
thais <thaisviana081@outlook.com>
2022-06-14 13:14:37 -0300

    por fim, crie uma array de compromissos e use a função map para que cada item do array apareça, como um item de lista, no seu componente App.

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -1,21 +1,39 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
+ */
 
-const Task = (value) => {
+/* const Task = (value) => {
   return (
     <li>{value}</li>
   );
-}
+} */
+
+// 2 
+// https://pt.stackoverflow.com/questions/514407/para-que-serve-e-como-definir-a-prop-key-no-react
+// https://pt-br.reactjs.org/docs/lists-and-keys.html
+// https://blog.matheuscastiglioni.com.br/listando-tarefas-com-react/
+
+/* const tasks = () => {
+  const tarefas = ["Acordar", "Tomar café", "Escovar os dentes", "Ir trabalhar"];
+  return (
+  <ul> 
+    {tarefas.map(position => <li>{position}</li>)}
+  </ul>
+  );
+};
+
 
 const element = Task("tarefas");
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {tasks}
         <p>
-          {element}
+          {tasks}
         </p>
         <a
           className="App-link"
@@ -23,11 +41,32 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        {tasks}
         </a>
+
       </header>
     </div>
   );
+}
+ */
+// ----------------------------------------------------------------------------------------
+// outra forma
+
+
+import React from 'react';
+import './App.css';
+
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>
+        {tarefas.map(position => <li>{position}</li>)}
+      </ul>
+    );
+  }
 }
 
 export default App;
