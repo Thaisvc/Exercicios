2bea0fb125a5aa9b0ec757b91824ba4ee2d90e80
thais <thaisviana081@outlook.com>
2022-06-20 15:23:31 -0300

    atividades

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -10,3 +10,7 @@ function App() {
 }
 
 export default App;
+
+/* 🚀 Crie uma aplicação React na sua máquina via create-react-app com o nome fixation-exercises-10-2. Crie um arquivo Image.js no diretório src do projeto e copie para esse arquivo o código escrito acima. Uma vez feito isso tudo, responda:
+Qual o nome do componente declarado acima?
+Chame o componente Image dentro do componente App, de forma que seja mostrada esta imagem, ou o texto Cute cat staring, caso a imagem não consiga ser carregada. */
