5289ebc6c60594766ef2a25bc04c8b499d82b906
thais <thaisviana081@outlook.com>
2022-06-21 09:42:40 -0300

    pokemos

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Pokemon from './Pokemon';
+import data from './data';
 
 function App() {
   return (
     <>
-    
+      <section className='container-cards'>
+        {
+          data.map(avatar => <Pokemon key={avatar.id} pokem={avatar} />)
+        }
+      </section>
     </>
   );
 }
--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Image from './image';
 class Pokemon extends React.Component {
     render() {
         return (
-            <> 
-
+            <>
+                <div className='card'>
+                    <p> Nome:{" "}{this.props.pokem.name}</p>
+                    <p> tipo:{" "}{this.props.pokem.type}</p>
+                    <p> Peso:{" "}{this.props.pokem.averageWeight.value}{" "}{this.props.pokem.averageWeight.measurementUnit}</p>
+                    <Image source={this.props.pokem.image} alternativeText="avatar" />
+                </div>
             </>
         )
     }
