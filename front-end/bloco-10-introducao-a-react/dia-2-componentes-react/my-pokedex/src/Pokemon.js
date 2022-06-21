08985fa476c0da27ef576a344430d52b9d726e9a
thais <thaisviana081@outlook.com>
2022-06-21 08:50:51 -0300

    Pokemon  Esse componente recebe como entrada um objeto que contém informações referentes a um pokemon específico

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pokemon extends React.Component {
+    render() {
+        return (
+            <> 
+
+            </>
+        )
+    }
+}
+
+export default Pokemon;
