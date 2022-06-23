805cca61bf4c14f9d7a4e7c06ebddde9b236e67c
thais <thaisviana081@outlook.com>
2022-06-23 09:28:35 -0300

    3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import React from 'react';
-
-function buttonA(){
-  console.log("clicou no A");
-}
-
-function buttonB(){
-  console.log("clicou no B");
-}
-function buttonC(){
-  console.log("clicou no C");
-}
-
 class App extends React.Component {
+ 
+
+  buttonA() {
+    console.log(this);
+    console.log("clicou no A");
+  }
+
+  buttonB() {
+    console.log(this);
+    console.log("clicou no B");
+  }
+
+  buttonC() {
+    console.log(this);
+    console.log("clicou no C");
+  }
   render() {
     return (
       <>
-      <button onClick={buttonA}>Button A</button>
-      <button onClick={buttonB}>Button B</button>
-      <button onClick={buttonC}>Button C</button>
+        <button onClick={buttonA}>Button A</button>
+        <button onClick={buttonB}>Button B</button>
+        <button onClick={buttonC}>Button C</button>
       </>
     )
   }
