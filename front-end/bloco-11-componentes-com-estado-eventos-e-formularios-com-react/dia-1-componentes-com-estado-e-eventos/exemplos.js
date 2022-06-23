cd8606d9062d423182bbb4d5175f5f2b86e39fd3
thais <thaisviana081@outlook.com>
2022-06-23 09:36:09 -0300

    exemplo Unindo componentes com estados e eventos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exemplos.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exemplos.js
@@ -49,3 +49,28 @@ class App extends React.Component {
   }
 }
 
+// Unindo componentes com estados e eventos
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      numeroDeCliques: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+  }
+}
