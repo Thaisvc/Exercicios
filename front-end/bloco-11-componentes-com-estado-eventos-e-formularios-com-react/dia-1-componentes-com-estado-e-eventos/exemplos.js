9ab1eeae57f6726c400df757cba8ee3950f94cb8
thais <thaisviana081@outlook.com>
2022-06-23 09:09:56 -0300

    Exemplo contructo, super,this,blind

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exemplos.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/exemplos.js
@@ -20,3 +20,32 @@ class App extends React.Component {
 }
 
 export default App;
+
+// Vinculando funções à classe com this e bind no constructor
+/* Toda classe em JavaScript tem acesso a um método chamado constructor(), e com as classes de React, definidas com class MinhaClasse extends React.Component, não é diferente! Quando um componente React é criado, ou seja, quando é colocado na tela do navegador, a primeira coisa que é executada é a função constructor(). Toda a lógica interna que o React adiciona aos seus componentes começa a ser inclusa neles nesse momento.
+No JavaScript, o super() refere-se ao construtor da classe pai. (No nosso caso, refere-se à implementação de React.Component.) É importante lembrar que você não pode usar o this em um construtor até que você tenha chamado o construtor da classe pai, o JavaScript não vai te deixar fazer isso. Então, por hora, apenas lembre-se que para usar o this dentro do constructor() é preciso chamar o super() antes. Ter isso em mente vai ser muito importante ao lidar com estados nas classes!
+A grande questão, no entanto, é que é possível adicionar aos construtores das classes React comportamentos e lógica extras! 
+O this acessa, nos componentes React, um objeto que guarda tudo que importa àquele componente. Um código de Hello, World! em React, ilustrado abaixo, gera a impressão no console a seguir:
+*/
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
+    console.log(this)
+    console.log('Clicou!')
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>Meu botão</button>
+  }
+}
+
