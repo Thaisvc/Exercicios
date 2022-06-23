3a67c76f67e7075035c3bf4eaf87c068c2f1efe1
thais <thaisviana081@outlook.com>
2022-06-23 09:05:29 -0300

    2- Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log().

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons./src/App.js
@@ -5,14 +5,20 @@ function buttonA(){
   console.log("clicou no A");
 }
 
-
+function buttonB(){
+  console.log("clicou no B");
+}
+function buttonC(){
+  console.log("clicou no C");
+}
 
 class App extends React.Component {
   render() {
     return (
       <>
       <button onClick={buttonA}>Button A</button>
-      
+      <button onClick={buttonB}>Button B</button>
+      <button onClick={buttonC}>Button C</button>
       </>
     )
   }
