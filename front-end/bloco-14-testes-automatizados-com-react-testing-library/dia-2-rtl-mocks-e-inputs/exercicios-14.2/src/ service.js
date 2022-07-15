72ad94c3f68da5a3f2e2964138bfcc6ef155314b
thais <thaisviana081@outlook.com>
2022-07-15 09:51:09 -0300

    Use a mesma função do primeiro exercício Utilize o mock e desenvolva uma nova implementação que receba três parâmetros Retorne a multiplicação dos parâmetros. Realize os testes que achar necessário. Resete sua implementação e crie uma nova, que receba apenas um parâmetro e retorne o dobro. Faça os testes que achar necessário. ㋡  ✔

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/ service.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/ service.js
@@ -1,3 +1,14 @@
-const numAleatorio = () => Math.floor(Math.random() * 101);
-module.exports = { numAleatorio };
+import React from 'react';
+
+class Services extends React.Component {
+    render() {
+        const numAleatorio = () => Math.floor(Math.random() * 100)
+        ;
+
+        return (<>
+        </>)
+    }
+}
+
+export default Services;
 
--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/service.test.js
@@ -3,10 +3,10 @@ const service = require('./ service');
 describe('Testando a services.js', () => {
 
   test('Testa se a função foi chamada, qual seu retorno e quantas vezes foi chamada.', () => {
-    service.numAleatorio = jest.fn().mockReturnValue(10)
-    expect(service.numAleatorio()).toBe(10)
-    expect(service.numAleatorio).toHaveBeenCalled();
-    expect(service.numAleatorio).toHaveBeenCalledTimes(1);
+    service.numAleatorio = jest.fn().mockReturnValue(10) // inicia com o valor 10
+    expect(service.numAleatorio()).toBe(10)  // verificase se o valor do resultado e = a 10
+    expect(service.numAleatorio).toHaveBeenCalled(); // verifica se a funçao foi chamada
+    expect(service.numAleatorio).toHaveBeenCalledTimes(1); // verifica se a funçao foi chamada 1 vez
   });
 });
 
@@ -18,6 +18,30 @@ describe('Testando a services.js  receber dois parâmetros e retornar a divisão
     expect(service.numAleatorio(10, 2)).toBe(5)
     expect(service.numAleatorio).toHaveBeenCalled();
     expect(service.numAleatorio).toHaveBeenCalledTimes(1);
-    expect(service.numAleatorio).toHaveBeenCalledWith(10,2);
+    expect(service.numAleatorio).toHaveBeenCalledWith(10,2); // verifica se a funçao foi chamada com os argumentos expecificos
   });
 });
+
+
+
+describe('testando implementações"', () => {
+  // Assim como o mockReturnValueOnce, podemos também implementar uma funcionalidade para apenas uma chamada com mockImplementationOnce.
+  test("mockando função para receber 3 parâmetros e retornar sua multiplicação", () => {
+      service.numAleatorio = jest.fn().mockImplementation((a,b,c) => a * b * c);
+      expect(service.numAleatorio(2,3,4)).toBe(24)
+      expect(service.numAleatorio).toHaveBeenCalled();
+      expect(service.numAleatorio).toHaveBeenCalledTimes(1);
+      expect(service.numAleatorio).toHaveBeenCalledWith(2,3,4);
+    });
+    test("mockando função que recebe um parâmetro e retorna seu dobro", () => {
+     service.numAleatorio.mockReset();
+      expect(service.numAleatorio).toHaveBeenCalledTimes(0);
+  
+     service.numAleatorio.mockImplementation(a => a * 2);
+  
+      expect(service.numAleatorio(5)).toBe(10);
+      expect(service.numAleatorio).toHaveBeenCalled();
+      expect(service.numAleatorio).toHaveBeenCalledTimes(1);
+      expect(service.numAleatorio).toHaveBeenCalledWith(5);
+    });
+  });
