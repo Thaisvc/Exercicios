e684c0edba094642bb4a0941a968a934ed2db439
thais <thaisviana081@outlook.com>
2022-07-15 09:33:43 -0300

    Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Defina o mock da função para a nova funcionalidade da função. Teste se a função foi chamada e a nova implementação de divisão foi aplicada. Verifique se a aplicação da nova implementação acontece apenas uma vez. ㋡  ✔

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-14.2/src/service.test.js
@@ -9,3 +9,15 @@ describe('Testando a services.js', () => {
     expect(service.numAleatorio).toHaveBeenCalledTimes(1);
   });
 });
+
+
+describe('Testando a services.js  receber dois parâmetros e retornar a divisão do primeiro pelo segundo.', () => {
+// Assim como o mockReturnValueOnce, podemos também implementar uma funcionalidade para apenas uma chamada com mockImplementationOnce.
+  test('testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
+    service.numAleatorio = jest.fn().mockImplementationOnce((a,b) => a / b);
+    expect(service.numAleatorio(10, 2)).toBe(5)
+    expect(service.numAleatorio).toHaveBeenCalled();
+    expect(service.numAleatorio).toHaveBeenCalledTimes(1);
+    expect(service.numAleatorio).toHaveBeenCalledWith(10,2);
+  });
+});
