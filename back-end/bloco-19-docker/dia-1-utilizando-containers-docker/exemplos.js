95fe538d54da227834997371c489ffabe0c0f30a
thais viana <thaisviana081@outlook.com>
2022-09-08 12:58:40 -0300

    Dia 19.1

---
+++ back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exemplos.js
@@ -0,0 +1,13 @@
+Revisão dos conceitos
+Quanta coisa nova sobre Docker em apenas um dia de conteúdo!
+Para te ajudar, vamos revisar todos os nomes e conceitos que aprendemos hoje:
+Docker: conjunto de ferramentas (Dameon, API, CLI) para gerenciar imagens e containers.
+Arquivo Dockerfile: arquivo com linguagem própria, com os passos necessários para criar uma nova imagem Docker usando o código-fonte de um projeto.
+Imagem Docker: é o projeto “compactado”, que foi construído de acordo com os passos contidos no arquivo Dockerfile. Pode ser usado como base para criar novas imagens Docker.
+Container: é a execução de projeto através da sua imagem Docker já construída anteriormente.
+Registry: é o local remoto onde podemos enviar e baixar imagens Docker. Um registry pode ser público ou privado.
+Docker Hub:
+É o registry oficial da empresa Docker Inc.
+Ele é público, porém possui alguns limites.
+É possível assinar o serviço para utilizá-lo como registry privado.
+
