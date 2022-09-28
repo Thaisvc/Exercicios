01f8daa605af9db9187cb1e872769c648d4df7ec
thais viana <thaisviana081@outlook.com>
2022-09-28 12:41:33 -0300

    6.3 Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/simpson.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/simpson.js
@@ -26,10 +26,22 @@ async function getSimpsonId(id) {
     return chosenSimpson
 }
 
+async function filterSimpsons(){
+    const fileContent = await fs.readFile('./simpsons.json', 'utf-8') // readFile faz a leitura do arquivo
+    const simpsons = JSON.parse(fileContent); 
+    const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6'); // remove o id 6 e 10 
+    await fs.writeFile('./simpsons.json', JSON.stringify(newArray)) // writeFile escreve no arquivo q indiquei
+
+}
+
+
+
+
 // A função main é apenas para termos um ponto de entrada centralizado para o nosso script
 async function main() {
     // await readAll();
-    getSimpsonId(3).then((simpson) => console.log(simpson)) //outra forma pode ser usado wait tbm = o readAll
+   // getSimpsonId(3).then((simpson) => console.log(simpson)) //outra forma pode ser usado wait tbm = o readAll
+   filterSimpsons()
 };
 
 main();
