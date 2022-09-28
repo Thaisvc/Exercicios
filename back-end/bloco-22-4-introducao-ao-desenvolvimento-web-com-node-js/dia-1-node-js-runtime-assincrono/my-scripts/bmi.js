98f0c99e9fd5a74d71f77bac5d5bea76122db35f
thais viana <thaisviana081@outlook.com>
2022-09-28 09:37:42 -0300

    3 🚀 Chegou a hora de tornar nosso código mais interativo! Vamos adicionar inputs de entrada para quem usar.
    Edite o código para que os valores de weight e height sejam informados pela pessoa ao responder as perguntas: “What’ your weight?” e “What’ your height?”, deve-se utilizar o pacote readline-sync.

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/bmi.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/my-scripts/bmi.js
@@ -1,6 +1,11 @@
-const weightInKg = 48; // Você pode utilizar o valor que desejar aqui
-const heightInCm = 155; // Você pode utilizar o valor que desejar aqui
+const ask = require('readline-sync');
 
+
+
+
+ /* const weightInKg = 48; // Você pode utilizar o valor que desejar aqui
+const heightInCm = 155; // Você pode utilizar o valor que desejar aqui
+ 
 function handleBMI(weight, height) {
   console.log(`Weight: ${weight}, Height: ${height}`);
 
@@ -10,13 +15,15 @@ function handleBMI(weight, height) {
   const bmi = height / heightSquared;
   
   return bmi;
-}
-
+} 
+ */
 // A função main é o ponto de partida do nosso programa
-function main() {
-  const bmi = handleBMI(weightInKg, heightInCm);
+/* function main() { */
+const askWeight =  ask.question('What your weight?');
+const askHeight =  ask.question('What your height?');
+const bmi = handleBMI(askWeight, askHeight);
 
-  console.log(`BMI: ${bmi.toFixed(2)}`);
-}
+ /* console.log(`BMI: ${bmi.toFixed(2)}`);
+ } 
 
-main();
+main();*/
