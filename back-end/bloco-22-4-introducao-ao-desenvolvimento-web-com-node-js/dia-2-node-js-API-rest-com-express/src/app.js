b7ef325cb6ee446350c14e20964ea2fc6ac67759
thais viana <thaisviana081@outlook.com>
2022-09-29 15:52:10 -0300

    7 Gabarito  😅

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
@@ -43,4 +43,15 @@ app.get('/filter/myActivities', (req, res) => {
     res.status(200).json({ activities: filteredActivities });
   });
 
+app.get('/search/myActivities', (req, res) => {
+    const { q } = req.query;
+    let filteredActivities = [];
+  
+    if (q) {
+      filteredActivities = myActivities.filter((activity) => activity.description.includes(q));
+    }
+  
+    res.status(200).json({ activities: filteredActivities });
+  });
+
 module.exports = app;
