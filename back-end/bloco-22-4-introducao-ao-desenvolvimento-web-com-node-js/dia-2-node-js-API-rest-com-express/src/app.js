6d00e12543e321a95aac29c483f1f80755b51162
thais viana <thaisviana081@outlook.com>
2022-09-29 14:44:44 -0300

    4 Crie um endpoint do tipo GET com a rota /myActivities/:id, que possa listar uma atividade do array por id   🤌

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const myActivities = require('./data/activities');
 
 const app = express();
 
 app.use(express.json());
 
 // ROTAS
+
+app.get('/myActivities/:id', (req, res) => {
+    // const { id } = req.params;
+    // req é o q recebo / res o que respondo
+    const findId = myActivities.find(({ id }) => id === Number(req.params.id)); // vai procurar o id q eu passar
+    res.status(200).json(findId);// vai mostrar responder com id q foi encontrado em findId
+});
 
 module.exports = app;
