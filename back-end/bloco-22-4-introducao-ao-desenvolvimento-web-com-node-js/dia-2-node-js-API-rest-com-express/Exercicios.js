196e9d5bab5241db83d3e8ab2dd6af4815b4608f
thais viana <thaisviana081@outlook.com>
2022-09-29 13:39:33 -0300

    01 Qual é a função do código de status 400 e 422?

---
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/Exercicios.js
@@ -0,0 +1,2 @@
+// 400- Bad Request Essa resposta significa que o servidor não entendeu a requisição pois está com uma sintaxe inválida.
+// 422- Unprocessable Entity A requisição está bem formada mas inabilitada para ser seguida devido a erros semânticos.
