5542ca2aaa49eb04bb1271f77debf242e08bc248
thais viana <thaisviana081@outlook.com>
2022-09-29 15:24:10 -0300

    GABARITO 6

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/src/app.js
@@ -20,11 +20,27 @@ app.get('/myActivities/', (req, res) => {
 // GABARITO OUTRA FORMA
 // app.get('/myActivities', (req, res) => res.status(200).json({ activities }));
 
-app.get('/filter/myActivities', (req, res) => {
+/* app.get('/filter/myActivities', (req, res) => {
     // const { status } = req.query;
     const exibirFiltro = myActivities.filter(({ status }) => status === req.query.status); 
     res.status(200).json(exibirFiltro);
     //  res.status(200).send(` o status é ${status}`);
-});
+}); */
+
+// GABARITO OUTRA FORMA 
+// app.js
+
+app.get('/filter/myActivities', (req, res) => {
+    //  Acesse a chave status usando req.query. Agora, inicia-se à variável com todas as atividades e aplicando uma condicional para verificar se o status repassado por meio do query params existe.
+    const { status } = req.query;
+    let filteredActivities = myActivities;
+  
+    if (status) {
+        // Terceiro passo: Se existir, aplica-se o método filter comparando o status por meio do operador de comparação === no array de objetos activities
+      filteredActivities = myActivities.filter((activity) => activity.status === status);
+    }
+  
+    res.status(200).json({ activities: filteredActivities });
+  });
 
 module.exports = app;
