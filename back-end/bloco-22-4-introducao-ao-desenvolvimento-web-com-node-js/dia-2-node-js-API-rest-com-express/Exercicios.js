8a53fcd635597f7ce17e4895a0dd475187c1f524
thais viana <thaisviana081@outlook.com>
2022-09-29 13:45:13 -0300

    Qual é a função do código de status 401?

--- back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/Exercicios.js
+++ back-end/bloco-22-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-rest-com-express/Exercicios.js
@@ -1,2 +1,3 @@
 // 400- Bad Request Essa resposta significa que o servidor não entendeu a requisição pois está com uma sintaxe inválida.
 // 422- Unprocessable Entity A requisição está bem formada mas inabilitada para ser seguida devido a erros semânticos.
+// 401 Unauthorized Embora o padrão HTTP especifique "unauthorized", semanticamente, essa resposta significa "unauthenticated". Ou seja, o cliente deve se autenticar para obter a resposta solicitada.
