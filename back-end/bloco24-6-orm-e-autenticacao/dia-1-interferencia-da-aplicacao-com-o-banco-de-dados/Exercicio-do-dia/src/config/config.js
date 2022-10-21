4df015ce373995791002bd912f8405a23d8f2550
thais viana <thaisviana081@outlook.com>
2022-10-20 21:07:22 -0300

    comecei tudo do 0

--- back-end/bloco24-6-orm-e-autenticacao/dia-1-interferencia-da-aplicacao-com-o-banco-de-dados/Exercicio-do-dia/src/config/config.js
+++ back-end/bloco24-6-orm-e-autenticacao/dia-1-interferencia-da-aplicacao-com-o-banco-de-dados/Exercicio-do-dia/src/config/config.js
@@ -1,28 +1,23 @@
-require("dotenv").config();
+require('dotenv').config();
+
+const config = {
+  username: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT ,
+  port: process.env.DB_PORT ,
+
+/*   username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'trybe_orm',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: process.env.DB_DIALECT || 'mysql',
+  port: process.env.DB_PORT || '33065', */
+};
 
 module.exports = {
-
-  development: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_ROOT_PASSWORD || 'root',
-    database: process.env.MYSQL_DATABASE || 'exercicio',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql', 
-    port: 33065,
-  },
-  test: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'root',
-    database: process.env.MYSQL_DATABASE || 'exercicio',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql', 
-
-  },
-  production: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'root',
-    database: process.env.MYSQL_DATABASE || 'exercicio',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql', 
-  },
+  development: config,
+  test: config,
+  production: config,
 };
