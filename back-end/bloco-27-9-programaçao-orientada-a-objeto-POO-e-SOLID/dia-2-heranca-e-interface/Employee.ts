7a5d726b7fa95dd0e9b01e166fa66be0f47c9626
thais viana <thaisviana081@outlook.com>
2022-11-15 10:25:38 -0300

    🚀 Exercício 3: Crie uma interface que representará uma pessoa funcionária.

---
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/Employee.ts
@@ -0,0 +1,17 @@
+/* `Interface`: Employee
+`Attributes`:
+    - registration: número do registro
+    - salary: valor do salário
+    - admissionDate: data de admissão
+`Methods`:
+    - generateRegistration: retorna uma string única gerada como registro */
+
+
+export default interface Employee {
+    registration: string;
+    salary: number;
+    admissionDate: Date;
+  
+    generateRegistration(): string;
+  }
+  
--- back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
@@ -35,3 +35,22 @@ console.log(fernando);
 
 // deve retornar erro
 // fernando.worksGrades = [45, 45, 45];
+
+// Para testar!
+// ./index.ts
+
+import Employee from './Employee';
+
+const testInterfaceEmployee: Employee = {
+  registration: 'FNC1234567891011',
+  salary: 1200.00,
+  admissionDate: new Date(),
+
+  generateRegistration(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+    return `FNC${randomStr}`;
+  },
+};
+
+console.log(testInterfaceEmployee);
