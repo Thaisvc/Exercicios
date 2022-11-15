58c180bb8d49e111772d8e3ed5d295cf353eda32
thais viana <thaisviana081@outlook.com>
2022-11-15 10:27:12 -0300

    🚀 Exercício 4: Crie uma classe cujos objetos representem uma disciplina lecionada na escola.

---
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/Subject.ts
@@ -0,0 +1,29 @@
+/* // ./Subject.ts
+`Class`: Subject
+`Attributes`:
+    - name: nome da disciplina
+`Methods`:
+    - Getters/Setters
+    - constructor: deve receber como parâmetro nome
+`Validations`:
+    - O nome tem que possuir no mínimo 3 caracteres */
+
+export default class Subject {
+    constructor(private _name: string) {
+      this.name = _name;
+    }
+  
+    get name(): string {
+      return this._name;
+    }
+  
+    set name(value: string) {
+      this.validateName(value);
+      this._name = value;
+    }
+  
+    private validateName(value: string): void {
+      if (value.length < 3) throw new Error('O nome deve conter no mínimo 3 caracteres.');
+    }
+  }
+  
--- back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
@@ -54,3 +54,20 @@ const testInterfaceEmployee: Employee = {
 };
 
 console.log(testInterfaceEmployee);
+
+
+// Para testar!
+// ./index.ts
+
+import Subject from './Subject';
+
+const math = new Subject('Matemática');
+const story = new Subject('História');
+const philosophy = new Subject('Filosofia');
+
+console.log(math);
+console.log(story);
+console.log(philosophy);
+
+// deve retornar erro
+// const invalidSubjectName = new Subject('Po');
