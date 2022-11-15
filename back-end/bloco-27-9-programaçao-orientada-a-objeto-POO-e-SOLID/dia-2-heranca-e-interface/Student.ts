ea39fc73f7f224da4e690cc6cd3f54f0258d16a7
thais viana <thaisviana081@outlook.com>
2022-11-15 10:24:03 -0300

    🚀 Exercício 2: Refatore nossa classe de pessoa estudante para que ela herde da nossa classe pessoa.

---
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/Student.ts
@@ -0,0 +1,71 @@
+// ./Student.ts
+/* 
+`Class`: Student
+`Extends`: Person
+`Attributes`:
+    - enrollment: matrícula da pessoa estudante
+    - examsGrades: notas de provas
+    - worksGrades: notas de trabalhos
+`Methods`:
+    - Getters/Setters
+    - constructor: deve receber como parâmetro nome e data de nascimento e
+      preencher a matrícula automaticamente
+    - sumGrades: retorna a soma das notas da pessoa estudante
+    - sumAverageGrade: retorna a média das notas da pessoa estudante
+    - generateEnrollment: retorna uma string única gerada
+      como matrícula para a pessoa estudante
+`Validations`:
+    - A matrícula deve possuir no mínimo 16 caracteres
+    - A pessoa estudante deve possuir no máximo 4 notas de provas
+    - A pessoa estudante deve possuir no máximo 2 notas de trabalhos */
+
+import Person from "./Person";
+
+export default class Student extends Person {
+  private _enrollment = String();
+  private _examsGrades: number[] = [];
+  private _worksGrades: number[] = [];
+
+  constructor(name: string, birthDate: Date) {
+    super(name, birthDate);
+    this.enrollment = this.generateEnrollment();
+  }
+
+  get enrollment(): string {
+    return this._enrollment;
+  }
+
+  //esse método checa se a inscrição da pessoa estudante possui no mínimo 16 caracteres
+  set enrollment(value: string) {
+    if (value.length < 16) throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
+
+    this._enrollment = value;
+  }
+
+  get examsGrades(): number[] {
+    return this._examsGrades;
+  }
+
+  set examsGrades(value: number[]) {
+    if (value.length > 4) throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+
+    this._examsGrades = value;
+  }
+
+  get worksGrades(): number[] {
+    return this._worksGrades;
+  }
+
+  set worksGrades(value: number[]) {
+    if (value.length > 2) throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos.');
+
+    this._worksGrades = value;
+  }
+
+  //esse método gera um id de inscrição aleatório baseado na data de cadastro da pessoa estudante
+  generateEnrollment(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+    return `STU${randomStr}`;
+  }
+}
