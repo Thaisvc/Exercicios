88ecc28fb8623fc56ff289df21789dc4f89efde6
thais viana <thaisviana081@outlook.com>
2022-11-15 10:29:09 -0300

    Exercício 5: Crie uma classe cujos objetos representem uma pessoa professora.

--- back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
+++ back-end/bloco-27-9-programaçao-orientada-a-objeto-POO-e-SOLID/dia-2-heranca-e-interface/index.ts
@@ -71,3 +71,30 @@ console.log(philosophy);
 
 // deve retornar erro
 // const invalidSubjectName = new Subject('Po');
+
+
+// Para testar!
+// ./index.ts
+
+import Teacher from './Teacher';
+
+const math1 = new Subject('Matemática');
+const history = new Subject('História');
+const philosophy1 = new Subject('Filosofia');
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, philosophy);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
+
+// deve retornar erro
+//const invalidTeacherSalary = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+
+// deve retornar erro
+// const today = new Date();
+// today.setDate(today.getDate() + 1)
+// const tomorrow = today;
+// marta.admissionDate = tomorrow;
