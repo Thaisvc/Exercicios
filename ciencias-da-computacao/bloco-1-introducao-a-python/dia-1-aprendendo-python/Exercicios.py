734b41533043b44d61cf59ce3be7943ad36904fc
thais viana <thaisviana081@outlook.com>
2023-02-13 15:48:27 -0300

    Exercício 6: Crie uma função que receba os três lado de um triângulo e informe qual o tipo de triângulo formado ou "não é triangulo", caso não seja possível formar um triângulo.

--- ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
+++ ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
@@ -49,3 +49,21 @@ def paint_costs(area):
     required_liters = area / 3
     required_cans = math.ceil(required_liters / 18)
     return required_cans, required_cans * can_price
+
+
+
+
+def type_of_triangle(side1, side2, side3):
+    is_triangle = (
+            side1 + side2 > side3 and
+            side2 + side3 > side1 and
+            side1 + side3 > side2
+    )
+    if not is_triangle:
+        return "não é triângulo"
+    elif side1 == side2 == side3:
+        return "equilátero"
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        return "isósceles"
+    else:
+        return "escaleno"
