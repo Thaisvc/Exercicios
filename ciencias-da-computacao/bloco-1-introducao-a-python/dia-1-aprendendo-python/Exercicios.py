f72592062df40da39817d1d1cb6c3e991b44ead7
thais viana <thaisviana081@outlook.com>
2023-02-13 15:45:01 -0300

    Exercício 5: Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Crie uma função que retorne dois valores em uma tupla contendo a quantidade de latas de tinta a serem compradas e o preço total a partir do tamanho de uma parede (em m²).

--- ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
+++ ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
@@ -28,3 +28,13 @@ def find_biggest_name(names):
         if len(name) > len(biggest_name):
             biggest_name = name
     return biggest_name
+
+
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = required_liters // 18
+    if required_liters % 18:
+        required_cans += 1
+    return required_cans, required_cans * can_price
