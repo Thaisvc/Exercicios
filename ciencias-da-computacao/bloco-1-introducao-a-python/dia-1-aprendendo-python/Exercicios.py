fda60e6ada63a73ce37da9c44c3aa5a789934a73
thais viana <thaisviana081@outlook.com>
2023-02-13 15:47:39 -0300

    06 outra opç

--- ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
+++ ciencias-da-computacao/bloco-1-introducao-a-python/dia-1-aprendendo-python/Exercicios.py
@@ -1,3 +1,5 @@
+import math
+
 def maior (a,b):
     if a >= b :
         return a 
@@ -37,4 +39,13 @@ def paint_costs(area):
     required_cans = required_liters // 18
     if required_liters % 18:
         required_cans += 1
+    return required_cans, required_cans * can_price
+
+
+# Uma alternativa mais direta, utilizando o módulo math
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = math.ceil(required_liters / 18)
     return required_cans, required_cans * can_price
