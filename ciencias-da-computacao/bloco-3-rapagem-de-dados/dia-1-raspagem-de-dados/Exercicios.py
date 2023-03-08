2619aef491b6cbdfb6cd325c69ec4fbf3dd04d73
thais viana <thaisviana081@outlook.com>
2023-03-08 15:57:46 -0300

    Exercício 7

--- ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
+++ ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
@@ -62,3 +62,17 @@ with MongoClient() as client:
     db = client.library
     for book in db.books.find({"categories": category}, projection=["title"]):
         print(book["title"])
+        
+
+#7
+with MongoClient() as client:
+    db = client.library
+    pipelines = [
+        {"$match": {"status": "PUBLISH"}},
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ]
+    for category in db.books.aggregate(pipelines):
+        print(category["_id"], category["count"], sep=": ")       
+        
