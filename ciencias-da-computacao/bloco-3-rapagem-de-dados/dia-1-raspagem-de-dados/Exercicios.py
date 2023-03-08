04e2cb7a28129b7e6c48aa9a091b6c3f65518bb4
thais viana <thaisviana081@outlook.com>
2023-03-08 15:34:12 -0300

    Exercício 3

--- ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
+++ ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
@@ -1,8 +1,13 @@
 import requests
-response = requests.get("https://httpbin.org/encoding/utf8")
+""" response = requests.get("https://httpbin.org/encoding/utf8")
 print(response.text) 
 
 response = requests.get("https://api.github.com/users")
 users = response.json()
 for user in users:
-    print(f"{user['login']} {user['url']}")
+    print(f"{user['login']} {user['url']}") """
+
+
+response = requests.get( "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+    headers={"User-agent": "Mozilla", "Accept": "text/html"},)
+assert "bot detected" not in response.text
