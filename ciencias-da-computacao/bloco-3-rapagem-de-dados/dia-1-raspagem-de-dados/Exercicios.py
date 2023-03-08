c915e00a5d65c5786943f6740632f18127c02d0a
thais viana <thaisviana081@outlook.com>
2023-03-08 15:29:18 -0300

    Exercício 2

--- ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
+++ ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
@@ -1,4 +1,8 @@
 import requests
-
 response = requests.get("https://httpbin.org/encoding/utf8")
-print(response.text)
+print(response.text) 
+
+response = requests.get("https://api.github.com/users")
+users = response.json()
+for user in users:
+    print(f"{user['login']} {user['url']}")
