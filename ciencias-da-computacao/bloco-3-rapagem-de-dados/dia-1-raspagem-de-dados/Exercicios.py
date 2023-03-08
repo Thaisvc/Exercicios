34e71a921286734137b57024c445f3eaf15b5da4
thais viana <thaisviana081@outlook.com>
2023-03-08 15:53:44 -0300

    Exercício 4

--- ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
+++ ciencias-da-computacao/bloco-3-rapagem-de-dados/dia-1-raspagem-de-dados/Exercicios.py
@@ -1,13 +1,38 @@
 import requests
+import parsel
+
 """ response = requests.get("https://httpbin.org/encoding/utf8")
-print(response.text) 
+print(response.text)
 
 response = requests.get("https://api.github.com/users")
 users = response.json()
 for user in users:
-    print(f"{user['login']} {user['url']}") """
+    print(f"{user['login']} {user['url']}")
 
 
-response = requests.get( "https://scrapethissite.com/pages/advanced/?gotcha=headers",
-    headers={"User-agent": "Mozilla", "Accept": "text/html"},)
+response = requests.get(
+    "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+    headers={"User-agent": "Mozilla", "Accept": "text/html"},
+)
 assert "bot detected" not in response.text
+
+ """
+
+
+
+URL_BASE = "http://books.toscrape.com/catalogue/"
+response = requests.get("http://books.toscrape.com/catalogue/the-grand-design_405/index.html")
+selector = parsel.Selector(response.text)
+
+title = selector.css("h1::text").get()
+
+price = selector.css(".product_main > .price_color::text").re_first(r"\d*\.\d{2}")
+
+description = selector.css("#product_description ~ p::text").get()
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
+
+cover = URL_BASE + selector.css("#product_gallery img::attr(src)").get()
+
+print(title, price, description, cover, sep=",")
