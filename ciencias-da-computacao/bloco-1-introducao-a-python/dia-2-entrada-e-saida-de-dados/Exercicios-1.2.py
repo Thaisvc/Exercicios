17417034821eadbccd00f2fd7d55ed39a4fbb949
thais viana <thaisviana081@outlook.com>
2023-03-23 14:43:26 -0300

    Exercício 2: Jogo da palavra embaralhada. Desenvolva um jogo em que a pessoa usuária tenha que adivinhar uma palavra que será mostrada com as letras embaralhadas. O programa terá uma lista de palavras e escolherá uma aleatoriamente. O jogador terá três tentativas para adivinhar a palavra. Ao final, a palavra deve ser mostrada na tela, informando se a pessoa ganhou ou perdeu o jogo.
    🦜 Para embaralhar uma palavra faça: scrambled_word = "".join(random.sample(word, len(word)))
    
    🦜 O sorteio de uma palavra aleatória pode ser feito utilizando o método choice: random.choice(["word1", "word2", "word3"]) -> "word2".

--- ciencias-da-computacao/bloco-1-introducao-a-python/dia-2-entrada-e-saida-de-dados/Exercicios-1.2.py
+++ ciencias-da-computacao/bloco-1-introducao-a-python/dia-2-entrada-e-saida-de-dados/Exercicios-1.2.py
@@ -1,3 +1,6 @@
+import random
+
+
 def vertical_inverted_ladder(word):
     for removed_letters in range(len(word)):
         for index in range(len(word) - removed_letters):
@@ -7,3 +10,45 @@ def vertical_inverted_ladder(word):
     if __name__ == "__main__":
         name = input("Digite um nome: ")
     vertical_inverted_ladder(name)
+
+
+WORDS = [
+    "cat",
+    "elephant",
+    "dog",
+    "monkey",
+    "duck",
+    "chameleon",
+    "bear",
+    "moose",
+    "rooster",
+]
+MAX_ATTEMPTS = 3
+
+
+def draw_secret_word(words):
+    secret_word = random.choice(words)
+    scrambled_word = "".join(random.sample(secret_word, len(secret_word)))
+    return secret_word, scrambled_word
+
+
+def collect_guesses():
+    guesses = []
+    for attempt in range(MAX_ATTEMPTS):
+        guess = input("Guess the word: ")
+        guesses.append(guess)
+    return guesses
+
+
+def check_game_result(secret_word, guesses):
+    if secret_word in guesses:
+        print(f"You win: {secret_word}")
+    else:
+        print(f"You lose: {secret_word}")
+
+
+if __name__ == "__main__":
+    secret_word, scrambled_word = draw_secret_word(WORDS)
+    print(f"Scrambled word is {scrambled_word}")
+    guesses = collect_guesses()
+    check_game_result(secret_word, guesses)
