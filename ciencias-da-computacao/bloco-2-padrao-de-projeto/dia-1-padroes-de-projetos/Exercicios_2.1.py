b5613d364dafe575e64e3339eae12a1f6bc09bcf
thais viana <thaisviana081@outlook.com>
2023-03-23 14:59:32 -0300

    Exercício 3

--- ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-1-padroes-de-projetos/Exercicios_2.1.py
+++ ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-1-padroes-de-projetos/Exercicios_2.1.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from abc import ABC, abstractmethod
+from math import pi as PI
 
 
 class TV:
@@ -44,3 +46,47 @@ class Estatistica:
     def moda(cls, numbers):
         number, _ = Counter(numbers).most_common()[0]
         return number
+
+
+class FiguraGeometrica(ABC):
+    @abstractmethod
+    def area(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def perimetro(self):
+        raise NotImplementedError
+
+
+class Quadrado(FiguraGeometrica):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado * self.lado
+
+    def perimetro(self):
+        return 4 * self.lado
+
+
+class Retangulo(FiguraGeometrica):
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+    def perimetro(self):
+        return 2 * (self.base + self.altura)
+
+
+class Circulo(FiguraGeometrica):
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return PI * self.raio * self.raio
+
+    def perimetro(self):
+        return 2 * PI * self.raio
