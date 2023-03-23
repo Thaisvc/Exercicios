771e247edbc408996e136f0e0a27436650eedeea
thais viana <thaisviana081@outlook.com>
2023-03-23 14:58:49 -0300

    Exercício 2

--- ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-1-padroes-de-projetos/Exercicios_2.1.py
+++ ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-1-padroes-de-projetos/Exercicios_2.1.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class TV:
     def __init__(self, tamanho):
         self.__volume = 50
@@ -15,9 +18,29 @@ class TV:
 
     def modificar_canal(self, canal):
         if canal <= 1 or canal >= 99:
-            raise ValueError('Canal indisponível')
+            raise ValueError("Canal indisponível")
 
         self.__canal = canal
 
     def ligar_desligar(self):
         self.__ligada = not self.__ligada
+
+
+class Estatistica:
+    @classmethod
+    def media(cls, numbers):
+        return sum(numbers) / len(numbers)
+
+    @classmethod
+    def mediana(cls, numbers):
+        numbers.sort()
+        index = len(numbers) // 2
+        if len(numbers) % 2 == 0:
+            return (numbers[index - 1] + numbers[index]) / 2
+
+        return numbers[index]
+
+    @classmethod
+    def moda(cls, numbers):
+        number, _ = Counter(numbers).most_common()[0]
+        return number
