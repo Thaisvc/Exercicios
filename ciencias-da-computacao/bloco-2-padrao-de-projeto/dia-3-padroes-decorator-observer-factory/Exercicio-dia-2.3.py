4a3e2e9a06b7d46c9ec0498f92a8fe44ec5a5e0c
thais viana <thaisviana081@outlook.com>
2023-03-27 10:52:19 -0300

    Exercício 2

--- ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-3-padroes-decorator-observer-factory/Exercicio-dia-2.3.py
+++ ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-3-padroes-decorator-observer-factory/Exercicio-dia-2.3.py
@@ -37,3 +37,77 @@ logger = Log()
 print(LogSuccess(logger).dispara_log("O sistema esta funcionando"))
 print(LogWarning(logger).dispara_log("O sistema esta lento"))
 print(LogError(logger).dispara_log("O sistema esta com erros"))
+
+
+# 2
+
+
+from abc import ABC, abstractmethod
+
+
+class Alarme:
+    def __init__(self):
+        self.__rotinas = []
+
+    def adicionar_rotinas(self, rotina):
+        self.__rotinas.append(rotina)
+
+    def acionar_todas_rotinas(self):
+        for rotina in self.__rotinas:
+            rotina.acionar()
+
+    def despertar(self):
+        print("Som do alarme: Triiiiiiim, Triiiiiiim")
+        self.acionar_todas_rotinas()
+
+
+# Interface Observer
+class Acionador(ABC):
+    @abstractmethod
+    def acionar(self):
+        pass
+
+
+# Observador Rotina Luzes
+class AcionadorLuzes(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Acendendo as Luzes")
+
+
+# Observador Cafeteira
+class AcionadorCafeteira(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Preparando o café")
+
+
+# Observador Computador
+class AcionadorComputador(Acionador):
+    def __init__(self, alarme):
+        self.alarme = alarme
+        self.alarme.adicionar_rotinas(self)
+
+    def acionar(self):
+        print("Ligar computador")
+
+
+# Cliente
+if __name__ == "__main__":
+    alarme_manha = Alarme()
+    AcionadorLuzes(alarme_manha)
+    AcionadorCafeteira(alarme_manha)
+    AcionadorComputador(alarme_manha)
+
+    alarme_manha.despertar()
+
+    alarme_tarde = Alarme()
+    AcionadorCafeteira(alarme_tarde)
+
+    alarme_tarde.despertar()
