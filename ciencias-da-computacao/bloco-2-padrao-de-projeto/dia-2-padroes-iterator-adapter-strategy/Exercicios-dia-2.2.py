93cd866d9af9b40aafc4c19117f1f67282637c2d
thais viana <thaisviana081@outlook.com>
2023-03-27 10:41:37 -0300

    Exercício 3

--- ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-2-padroes-iterator-adapter-strategy/Exercicios-dia-2.2.py
+++ ciencias-da-computacao/bloco-2-padrao-de-projeto/dia-2-padroes-iterator-adapter-strategy/Exercicios-dia-2.2.py
@@ -82,3 +82,25 @@ class Baralho(Iterable):
         return f"{[carta for carta in self]}"
     
     
+    
+    
+# 3
+
+class IteradorDoBaralhoInverso(Iterator):
+    def __init__(self, cartas):
+        self._cartas = cartas
+        self._pos = - 1
+
+    def __next__(self):
+        try:
+            carta = self._cartas[self._pos]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._pos -= 1
+            return carta
+
+
+class BaralhoInverso(Baralho):
+    def __iter__(self):
+        return IteradorDoBaralhoInverso(self._cartas)
