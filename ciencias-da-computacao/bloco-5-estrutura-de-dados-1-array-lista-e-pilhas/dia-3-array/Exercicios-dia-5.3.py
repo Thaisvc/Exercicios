d384a2437abf7a803bb8bb9d074fcea1366cbd91
thais viana <thaisviana081@outlook.com>
2023-03-31 13:04:39 -0300

    exercicio 2 comentado

--- ciencias-da-computacao/bloco-5-estrutura-de-dados-1-array-lista-e-pilhas/dia-3-array/Exercicios-dia-5.3.py
+++ ciencias-da-computacao/bloco-5-estrutura-de-dados-1-array-lista-e-pilhas/dia-3-array/Exercicios-dia-5.3.py
@@ -22,3 +22,33 @@ def verify_max_time_ok(collected_values):
     return max_time
 
 # O algoritmo realiza um for,portanto possui Complexidade de Tempo O(n).
+
+
+def shuffle(items):
+    answer = []
+    div_cards_by_two = len(items) // 2  # divide a lista ao meio exemplo
+    # de lista [1, 4, 4, 7, 6, 6]
+    print(div_cards_by_two)  # saida 3 meio da lista
+    # range vai receber um numero 3 q e o meio da lista e vai
+    # gerar para o  offset um numero de 0,1,2 = 3 q são index
+    for offset in range(div_cards_by_two):
+        # a cada iteraçao offset muda indicando um index na lista items
+        # entao primeira  iteraçao index 0 numero 1 da lista q e fatiada pelo :
+        # e de novo : fatiada para pegar o 3 numero da lista (div_cards_by_two)
+        # q e o 7
+        answer.extend(items[offset::div_cards_by_two])
+        # resutaldo final [1, 7, 4, 6, 4, 6]
+
+    return answer
+
+
+""" Ao usar lista[começo:fim:passo] nós temos “slices”, pedaços da lista que
+começam em começo (por padrão 0), terminam em fim (por padrão, até o 
+fim da lista) 
+e pula de passo em passo (por padrão, 1). 
+ou seja, [11, 12, 21, 22, 31, 32][::2]
+resulta em [11, 21, 31].
+
+Resposta da análise de complexidade
+
+O algoritmo realiza um for, portanto possui Complexidade de Tempo O(n). """
