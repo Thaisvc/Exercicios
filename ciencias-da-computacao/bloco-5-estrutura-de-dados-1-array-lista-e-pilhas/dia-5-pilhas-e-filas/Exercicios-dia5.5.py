75643e46c454e00f68533d6399ea6fa4f34eaefa
thais viana <thaisviana081@outlook.com>
2023-04-04 10:06:06 -0300

    Exercício 2

--- ciencias-da-computacao/bloco-5-estrutura-de-dados-1-array-lista-e-pilhas/dia-5-pilhas-e-filas/Exercicios-dia5.5.py
+++ ciencias-da-computacao/bloco-5-estrutura-de-dados-1-array-lista-e-pilhas/dia-5-pilhas-e-filas/Exercicios-dia5.5.py
@@ -24,3 +24,26 @@ class Queue:
 
     def peek(self):
         return self.__data.get_element_at(0)
+
+
+""" Exercício 2:
+Pilhas: crie uma classe Stack, onde deve conter as 
+operações: push, pop, peek e is_empty. """
+
+
+class Stack:
+
+    def __init__(self):
+        self.__data = LinkedList()
+
+    def is_empty(self):
+        return not len(self.__data)
+
+    def push(self, value):
+        self.__data.insert_last(value)
+
+    def pop(self):
+        return self.__data.remove_last()
+
+    def peek(self):
+        return self.__data.get_element_at(len(self.__data))
