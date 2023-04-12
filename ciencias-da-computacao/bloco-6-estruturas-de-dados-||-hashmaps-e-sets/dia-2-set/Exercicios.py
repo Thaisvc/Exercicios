ce9140476863137d31172bc248ffb272e949892c
thais viana <thaisviana081@outlook.com>
2023-04-12 10:59:26 -0300

    Exercício 2 - Substring Dada uma string, ache o tamanho da maior substring que não tenha caracteres repetidos. Complexidade de tempo limite aceitável: O(n²).

--- ciencias-da-computacao/bloco-6-estruturas-de-dados-||-hashmaps-e-sets/dia-2-set/Exercicios.py
+++ ciencias-da-computacao/bloco-6-estruturas-de-dados-||-hashmaps-e-sets/dia-2-set/Exercicios.py
@@ -84,3 +84,18 @@ if quem_ganhou:
     print(f"Quem ganhou: {quem_ganhou} ")
 else:
     print("Deu empate")
+
+
+
+
+def longer_no_repeating_substring_len(string):
+    biggest = 0
+    for index, _ in enumerate(string):
+        substr = set()
+        for letter in string[index:]:
+            if letter in substr:
+                break
+            substr.add(letter)
+        if len(substr) > biggest:
+            biggest = len(substr)
+    return biggest
